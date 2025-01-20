ccf46bc13f9c3108d7e01ae9d7ab055697af2bae
OJ Abba <phavorabba@gmail.com>
2025-01-20 17:33:49 +0000

    [Chore] SC-162578 SC-123527 Create `UserData` type

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -3,3 +3,24 @@ export interface ISettings {
   consumer_secret?: string;
   store_url?: string;
 }
+
+export interface DPUser {
+  id: string;
+  name: string;
+  firstName: string;
+  lastName: string;
+  titlePrefix: string;
+  isDisabled: boolean;
+  isAgent: boolean;
+  isConfirmed: boolean;
+  emails: string[];
+  primaryEmail: string;
+  customFields: Record<string, unknown>;
+  language: string;
+  locale: string;
+};
+
+
+export interface UserData {
+  user:DPUser
+}
